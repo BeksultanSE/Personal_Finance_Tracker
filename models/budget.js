9cd454cbb9ac9c42ddb42a5256a96522be76e394
BEKSULTAN <230599@astanait.edu.kz>
2025-02-18 17:42:29 +0500

    indexes for db

--- models/budget.js
+++ models/budget.js
@@ -6,6 +6,8 @@ const budgetSchema = new mongoose.Schema({
   limit: { type: Number, required: true },
 }, { collection: "budgets" });
 
+budgetSchema.index({ userId: 1, category: 1 }); // Compound index on userId and category
+
 const Budget = mongoose.model('Budget', budgetSchema);
 
 module.exports = Budget;
